ad03141ced600e25a87b40e044a532f2eb5468de
yanhaijun <15261882407@163.com>
2023-11-09 14:18:15 +0800

    catagory本身作用 对象下的功能 动态新增行为

---
+++ patternOfAdapter/patternOfAdapter/GamePad+Cheat.h
@@ -0,0 +1,18 @@
+//
+//  GamePad+Cheat.h
+//  patternOfAdapter
+//
+//  Created by haijunyan on 2023/11/8.
+//  Copyright © 2023 wupeng. All rights reserved.
+//
+
+#import "GamePad.h"
+
+@interface GamePad (Cheat)
+- (void)cheat;
+
+//千万不用重写方法
+//- (void)up;
+
+@end
+
